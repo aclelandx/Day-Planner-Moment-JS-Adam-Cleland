c279c23601228e867cb3a361f257037eefa8d258
aclelandx <95543064+aclelandx@users.noreply.github.com>
2022-02-08 07:02:30 -0500

    Finalized comments in the js file for better readability for future reference

--- assets/js/daily-planner.js
+++ assets/js/daily-planner.js
@@ -1,3 +1,5 @@
+// Declaring all my variables and dom elements that are put on the global scope.
+
 const $hero = document.getElementById(`hero-splash`);
 const $currentDate = document.getElementById(`current-date`);
 const $timeDisplayed = document.getElementById(`time-display`);
@@ -14,7 +16,8 @@ let currentAdditionLi = ``;
 $currentDate.textContent = moment().format(`MMMM D, Y`);
 $timeDisplayed.textContent = moment().format(`h:mm:ssA`);
 
-
+// first part of the adding to the list function, grabs the target that you clicked on and if it was the plus sign button,
+// then the popup is displayed and the global scope variables are reassigned, if the item was the span then the information is removed from the list.
 function addListContentPt1(e) {
     let target = e.target;
     let parent = e.currentTarget;
@@ -30,6 +33,7 @@ function addListContentPt1(e) {
     }
 };
 
+// on the popup screen this function takes the users input and saves it to the local storage attacked to a name reflecting the html items data type.
 function addListContentPt2() {
     let usersNewTask = $popupInput.value.trim();
 
@@ -43,6 +47,14 @@ function addListContentPt2() {
     }
 }
 
+// Logic for the x icon in the top right of the popup page, this sets the popup displays back to the default of none and resets the input values.
+function exitPopup() {
+    $popup.style = `display:none;`;
+    $popupInput.value = ``;
+    $popupInput.previousElementSibling.innerHTML = `Add your item to the space provided`;
+};
+
+// clears out the previous p tag so the information does not get duplicated then runs the fillInData function to populate the information.
 function updateData() {
     for (let i = 0; i < $hourByHourLi.length; i++) {
         $hourByHourLi[i].childNodes[1].remove();
@@ -50,12 +62,8 @@ function updateData() {
     fillInData();
 };
 
-function exitPopup() {
-    $popup.style = `display:none;`;
-    $popupInput.value = ``;
-    $popupInput.previousElementSibling.innerHTML = `Add your item to the space provided`;
-};
-
+// timed interval that fires every 1 second to be continuously updating the time that is displayed in the hero image,
+// this function is also responsible for color coating the li's for the hour blocks.
 let timeUpdate = setInterval(() => {
     let todaysDate = moment().format(`MMMM D, Y`);
     let currentTime = moment().format(`h:mm:ssA`);
@@ -74,6 +82,7 @@ let timeUpdate = setInterval(() => {
     }
 }, 1000);
 
+// this for loop with the if else if statement attached, takes all the information in including the current time and adds the users information from the localStorage.
 function fillInData() {
     for (let i = 0; i < $hourByHourLi.length; i++) {
 
@@ -98,13 +107,18 @@ function fillInData() {
     };
 };
 
-// fills in the data from the local storage
+// Adds the previous information that is found inside the localStorage so the page will be auto populated if it has been returned too.
 fillInData()
 
+// adds an event listener to each one of the Li's so event delegation may be used in the addListContentPt1
 for (let i = 0; i < $hourByHourLi.length; i++) {
     $hourByHourLi[i].addEventListener(`click`, addListContentPt1)
 };
+
+// adds the event listener for the exit popup button in the top right of the popup window.
 $popupClose.addEventListener(`click`, exitPopup);
+
+// adds the event listener for the submit button so the users data can be populated on the page and continue on to the next and final steps.
 $popupSubmit.addEventListener(`click`, addListContentPt2);
 
 
